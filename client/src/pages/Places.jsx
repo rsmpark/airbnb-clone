cb9b1fe576a75842a35585355159f6b9f1b91695
Rick Park <ricksmp95@gmail.com>
2023-06-27 14:32:13 +0100

    Fix img path issue.

--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -49,8 +49,14 @@ export default function Places() {
               className="flex gap-4 bg-gray-100 p-4 rounded-2xl cursor-pointer"
               key={place._id}
             >
-              <div className="w-32 h-32 bg-gray-300 grow shrink-0">
-                {place.photos.length > 0 && <img src={place.photos[0]} alt="place" />}
+              <div className=" flex w-32 h-32 bg-gray-300 grow shrink-0">
+                {place.photos.length > 0 && (
+                  <img
+                    className="object-cover"
+                    src={`http://localhost:4000/uploads/${place.photos[0]}`}
+                    alt="place"
+                  />
+                )}
               </div>
               <div className="grow-0 shrink">
                 <h2 className="text-xl">{place.title}</h2>
