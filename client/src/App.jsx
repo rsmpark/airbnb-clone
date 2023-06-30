4297b63ef44e8ebb028128ff8f9c4a7664ee8a82
Rick Park <ricksmp95@gmail.com>
2023-06-30 21:22:14 +0100

    Add enironment api url.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import Register from "./pages/Register";
 
 import "./App.css";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
